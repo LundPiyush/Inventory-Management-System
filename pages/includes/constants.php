d6abbeabc8d87e746a80c685e352882318fac5a5
Piyush Lund <piyushlund349@gmail.com>
2019-09-24 21:27:58 +0530

    final

---
+++ pages/includes/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("status-constants.php");
+
+define("SERVER","localhost");
+define("USER","root");
+define("PASSWORD","");
+define("DB","erp");
+
+define("BASE_SERVER","localhost");
+
+?>
